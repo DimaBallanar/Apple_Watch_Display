8c0cc9e57fa7c9f774373e0338e9d9cbe10881a2
Dima Ballanar <D.Ballanar@gmail.com>
2023-06-11 16:11:37 +0300

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function doWatch() {
+    const date = new Date();
+    const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
+    const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
+    const seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+    document.querySelector('.watch').innerHTML = hours + ':' + minutes + ':' + seconds;
+    document.querySelector('.date').innerHTML = date.getFullYear();
+}
+
+document.querySelector(".block");
+setInterval(doWatch, 1000);
