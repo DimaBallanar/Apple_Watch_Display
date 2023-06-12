bf2a7632ff2a7ef788074ea7bec40730ec4b9c6b
Dima Ballanar <D.Ballanar@gmail.com>
2023-06-12 16:44:15 +0300

    fix

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-function Watch() {
+unction Watch() {
     const date = new Date();
     const hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours();
     const minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();
